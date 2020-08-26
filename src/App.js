5c3d8510f755001d84d6771fe6f88f1848cf9937
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-08-26 20:47:13 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Styled components
+const Main = styled.div`
+    width: 100px;
+    height: 1000px;
+    background-color: red;
+`
+
+const App = () => {
+    return <Main></Main>
+}
+
+export default App
