a30901a7dc38f1bd23a167178a9846b7cb5ff664
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-08-28 23:43:38 +0200

    Added main menu container

--- src/App.js
+++ src/App.js
@@ -5,11 +5,46 @@ import styled from 'styled-components'
 const Main = styled.div`
     width: 100vw;
     height: 100vh;
-    background-color: #131313;
+    /* background-color: #131313; */
+    background-image: url('/appScreenshot.png');
+    background-size: contain;
+`
+const MenuButton = styled.button`
+    position: fixed;
+    top: 20px;
+    right: 60px;
+    width: 40px;
+    height: 40px;
+    background-color: transparent;
+    background-image: url('/assets/apps.svg');
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    border: none;
+    cursor: pointer;
+    overflow: hidden;
+    outline: none;
+`
+const Menu = styled.div`
+    top: 80px;
+    right: 30px;
+    position: fixed;
+    border: 1px solid #333333;
+    width: 400px;
+    height: 600px;
+    border-radius: 6px;
+    background-color: transparent;
+    backdrop-filter: blur(10px);
+    box-shadow: -23px 18px 33px -5px rgba(0, 0, 0, 0.7);
 `
 
 const App = () => {
-    return <Main></Main>
+    return (
+        <Main>
+            <MenuButton />
+            <Menu />
+        </Main>
+    )
 }
 
 export default App
